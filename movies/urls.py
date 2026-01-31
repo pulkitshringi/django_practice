b1c6095d3ce1eca0ac8e4b9a86b517aee7758dfb
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-01-31 17:33:58 +0530

    updated

--- movies/urls.py
+++ movies/urls.py
@@ -6,5 +6,5 @@ app_name = 'movies'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:movie_id>', views.detail, name='detail')
+    path('<int:movie_id>/', views.detail, name='detail')
 ]
